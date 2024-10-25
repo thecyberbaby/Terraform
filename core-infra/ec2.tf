5451f02d8c80720c0ff9f415e9e4303fece4a3b7
theycberbaby <nishantsharma958@gmail.com>
2024-10-26 03:24:06 +0530

    Adding userdata as a file

--- core-infra/ec2.tf
+++ core-infra/ec2.tf
@@ -10,21 +10,5 @@ resource "aws_instance" "jenkins_server" {
     Name = "dev-jenkins_server"
   }
 
-  user_data = <<-EOF
-    #!/bin/bash
-    # Install updates and required packages
-    sudo yum update -y
-    sudo amazon-linux-extras install java-openjdk11 -y
-
-    # Add Jenkins repository and key
-    wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-    
-    # Install Jenkins
-    sudo yum install jenkins -y
-    
-    # Start and enable Jenkins service
-    sudo systemctl start jenkins
-    sudo systemctl enable jenkins
-  EOF
+  user_data = file("../scripts/install-tools.sh")
 }
