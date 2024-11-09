839f62f56b68f32b7ebc22419d27781a9c5a2808
thecyberbaby <nishantsharma9358@gmail.com>
2024-11-10 03:45:25 +0530

    restructuing the code

--- core-infra/output.tf
+++
@@ -1,9 +0,0 @@
-output "jenkins_instance_public_ip" {
-  description = "The public IP address of the instance"
-  value       = aws_instance.jenkins_server.public_ip
-}
-
-output "jenkins_instance_public_dns" {
-  description = "The public DNS of the instance"
-  value       = aws_instance.jenkins_server.public_dns
-}
